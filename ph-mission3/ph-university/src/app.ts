93195823af9a440dccdbb26fa85b47bb12473bda
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-27 13:29:01 +0600

    refactor router

--- ph-mission3/ph-university/src/app.ts
+++ ph-mission3/ph-university/src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import { globalErrorHandler } from './app/middlwares/globalErrorHandler';
 import notFound from './app/middlwares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -12,8 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 //Appication routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Home Router');
