0ee9926c279aa9ddb2038f650bb7785d7b214ef2
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-02 18:44:34 +0600

    create academic semester constant variable

---
+++ ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.constant.ts
@@ -0,0 +1,28 @@
+import {
+  TAcademicSemesterName,
+  TAcademicSemesterCode,
+  TMonths,
+} from './academicSemester.interface';
+
+export const Months: TMonths[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const SemeterName: TAcademicSemesterName[] = [
+  'Autumn',
+  'Summer',
+  'Fall',
+];
+
+export const SemesterCode: TAcademicSemesterCode[] = ['01', '02', '03'];
