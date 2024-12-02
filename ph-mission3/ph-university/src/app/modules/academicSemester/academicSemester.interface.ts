153edad1cad91978de0e697c82820fe8c799cb2d
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-02 17:59:42 +0600

    change date of birth string to date type

---
+++ ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,8 @@
+export type AcademicSemester = {
+  name: 'Autumn' | 'Summer' | 'Fall';
+  code: '01' | '02' | '03';
+  year: Date;
+  startMonth: string;
+  endMonth: string;
+  syncId: string;
+};
