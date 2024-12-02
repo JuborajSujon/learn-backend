1f9fa54dd4d6e6c3d7d83ce0fe4bb09a9bda11fb
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-02 18:58:15 +0600

    define zod validation

---
+++ ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.validation.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import {
+  Months,
+  AcademicSemesterCode,
+  AcademicSemeterName,
+} from './academicSemester.constant';
+
+const createAcademicSemesterZodSchema = z.object({
+  body: z.object({
+    name: z.enum([...AcademicSemeterName] as [string, ...string[]], {
+      required_error: 'Academic semester is required',
+    }),
+    code: z.enum([...AcademicSemesterCode] as [string, ...string[]], {
+      required_error: 'Academic semester code is required',
+    }),
+    year: z.number({
+      required_error: 'Academic semester year is required',
+    }),
+    startMonth: z.enum([...Months] as [string, ...string[]], {
+      required_error: 'Academic semester start month is required',
+    }),
+    endMonth: z.enum([...Months] as [string, ...string[]], {
+      required_error: 'Academic semester end month is required',
+    }),
+  }),
+});
+
+export const AcademicSemesterValidations = {
+  createAcademicSemesterZodSchema,
+};
