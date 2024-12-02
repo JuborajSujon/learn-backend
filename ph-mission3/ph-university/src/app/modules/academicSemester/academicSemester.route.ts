54e38d59fbd5a93f97850699d3209d007d1c0d32
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-02 19:16:12 +0600

    define academic semester route

--- ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.route.ts
+++ ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { AcademicSemesterControllers } from './academicSemester.controller';
+import validateRequest from '../../middlwares/validateRequest';
+import { AcademicSemesterValidations } from './academicSemester.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  validateRequest(AcademicSemesterValidations.createAcademicSemesterZodSchema),
   AcademicSemesterControllers.createAcademicSemester,
 );
 
