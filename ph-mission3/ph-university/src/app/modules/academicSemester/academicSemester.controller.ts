338910e78c1e408b16271c58c9d3fc15c6ccbac7
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-03 07:03:04 +0600

    crud operation academic semester for controller

--- ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.controller.ts
+++ ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,6 +3,7 @@ import status from 'http-status';
 import catchAsync from '../../utils/CatchAsync';
 import { AcademicSemesterServices } from './academicSemester.service';
 
+// create academic semester controller
 const createAcademicSemester = catchAsync(async (req, res) => {
   // Will call service func to send this data
   const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
@@ -18,4 +19,51 @@ const createAcademicSemester = catchAsync(async (req, res) => {
   });
 });
 
-export const AcademicSemesterControllers = { createAcademicSemester };
+// get all academic semesters
+const getAllAcademicSemesters = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.getAllAcademicSemestersFromDB();
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Academic semesters are retrieved successfully',
+    data: result,
+  });
+});
+
+// get single academic semester
+const getSingleAcademicSemester = catchAsync(async (req, res) => {
+  const { semesterId } = req.params;
+  const result =
+    await AcademicSemesterServices.getSingleAcademicSemesterFromDB(semesterId);
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Academic semester is retrieved succesfully',
+    data: result,
+  });
+});
+
+// update academic semester
+const updateAcademicSemester = catchAsync(async (req, res) => {
+  const { semesterId } = req.params;
+  const result = await AcademicSemesterServices.updateAcademicSemesterIntoDB(
+    semesterId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: 'Academic semester is retrieved succesfully',
+    data: result,
+  });
+});
+
+export const AcademicSemesterControllers = {
+  createAcademicSemester,
+  getAllAcademicSemesters,
+  getSingleAcademicSemester,
+  updateAcademicSemester,
+};
