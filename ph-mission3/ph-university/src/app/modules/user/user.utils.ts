92cac91c46da3d0ee65515a1705dff8c3d22d66f
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-03 12:07:35 +0600

    Fix bug on creating ID

--- ph-mission3/ph-university/src/app/modules/user/user.utils.ts
+++ ph-mission3/ph-university/src/app/modules/user/user.utils.ts
@@ -6,12 +6,31 @@ export const findLastStudentId = async () => {
   const lastStudent = await User.findOne({ role: 'student' }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // Generate random id --> year semesterCode 4 digits
 export const generatedStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStuentId = await findLastStudentId();
+
+  if (lastStuentId) {
+    const lastStudnetSemesterYear = lastStuentId.substring(0, 4);
+
+    const lastStudnetSemesterCode = lastStuentId.substring(4, 6);
+
+    const currentSemesterYear = payload.year;
+    const currentSemesterCode = payload.code;
+
+    if (
+      lastStuentId &&
+      lastStudnetSemesterCode === currentSemesterCode &&
+      lastStudnetSemesterYear === currentSemesterYear
+    ) {
+      currentId = lastStuentId.substring(6);
+    }
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
