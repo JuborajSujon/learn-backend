04aee9b666761508a7d962ccd4448602d3de3820
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-03 07:41:45 +0600

    define route academic semester

--- ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.route.ts
+++ ph-mission3/ph-university/src/app/modules/academicSemester/academicSemester.route.ts
@@ -5,10 +5,27 @@ import { AcademicSemesterValidations } from './academicSemester.validation';
 
 const router = express.Router();
 
+// create academic semester route
 router.post(
   '/create-academic-semester',
   validateRequest(AcademicSemesterValidations.createAcademicSemesterZodSchema),
   AcademicSemesterControllers.createAcademicSemester,
+);
+
+// get all academic semesters route
+router.get('/', AcademicSemesterControllers.getAllAcademicSemesters);
+
+// get single academic semester route
+router.get(
+  '/:semesterId',
+  AcademicSemesterControllers.getSingleAcademicSemester,
+);
+
+// update academic semester route
+router.patch(
+  '/:semesterId',
+  validateRequest(AcademicSemesterValidations.createAcademicSemesterZodSchema),
+  AcademicSemesterControllers.updateAcademicSemester,
 );
 
 export const AcademicSemesterRoutes = router;
