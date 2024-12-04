d54ca53a351b2fa408343067338f939f5fdfc3b5
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-04 13:52:07 +0600

    13-10 Delete Student Using Transaction and Rollback

--- ph-mission3/ph-university/src/app/modules/student/student.service.ts
+++ ph-mission3/ph-university/src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose';
 import { Student } from './student.model';
+import AppError from '../../errors/AppError';
+import status from 'http-status';
+import { User } from '../user/user.model';
 
 const getAllStudentsFromDB = async () => {
   const result = await Student.find()
@@ -28,8 +32,48 @@ const getSingleStudentFromDB = async (id: string) => {
 };
 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true });
-  return result;
+  // check if student exists
+  const isStudentExist = await Student.findOne({ id });
+
+  if (!isStudentExist) {
+    throw new AppError(status.NOT_FOUND, 'Student not found');
+  }
+
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+
+    // delete student - transaction - 1
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedStudent) {
+      throw new AppError(status.NOT_FOUND, 'Student not found');
+    }
+
+    // delete user - transaction - 2
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedUser) {
+      throw new AppError(status.NOT_FOUND, 'User not found');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deletedStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
+  }
 };
 export const StudentServices = {
   getAllStudentsFromDB,
