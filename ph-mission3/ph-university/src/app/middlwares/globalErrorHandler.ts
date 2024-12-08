a13aac87a527e96ba81906eca8d7d2c1539f4e31
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-08 14:38:55 +0600

    14-3 How to convert Zod Error

--- ph-mission3/ph-university/src/app/middlwares/globalErrorHandler.ts
+++ ph-mission3/ph-university/src/app/middlwares/globalErrorHandler.ts
@@ -1,20 +1,63 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error.interface';
+import config from '../config';
 
-export const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+export const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'something went wrong';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'something went wrong';
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'something went wrong',
+    },
+  ];
+
+  const handleZodError = (error: ZodError) => {
+    const errorSources: TErrorSource = error.issues.map((issue: ZodIssue) => ({
+      path: issue?.path[issue.path.length - 1],
+      message: issue.message,
+    }));
+
+    const statusCode = 400;
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
+
+// Pattern for global error handler
+/**
+ success: boolean;
+ message: string;
+ error: [
+ path: string;
+ message: string;
+ ];
+ stack: string;
+ */
