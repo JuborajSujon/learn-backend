80d6f57ece0939c2603c4f69a0a17b9ef1ee273d
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-09 07:50:11 +0600

    14-5 How to Handle castError and 11000 error

--- ph-mission3/ph-university/src/app/errors/handleZodError.ts
+++ ph-mission3/ph-university/src/app/errors/handleZodError.ts
@@ -1,7 +1,10 @@
 import { ZodError, ZodIssue } from 'zod';
-import { TErrorSources } from '../interface/error.interface';
+import {
+  TErrorSources,
+  TGenericErrorResponse,
+} from '../interface/error.interface';
 
-export const handleZodError = (error: ZodError) => {
+export const handleZodError = (error: ZodError): TGenericErrorResponse => {
   const errorSources: TErrorSources = error.issues.map((issue: ZodIssue) => ({
     path: issue?.path[issue.path.length - 1],
     message: issue.message,
