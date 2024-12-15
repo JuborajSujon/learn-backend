3c6a19c57f91813a10a6eb8b9cc7a6f0529c2fac
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-15 22:53:36 +0600

    17-2 Implement business logic validation in auth service.

--- ph-mission3/ph-university/src/app/modules/auth/auth.service.ts
+++ ph-mission3/ph-university/src/app/modules/auth/auth.service.ts
@@ -2,9 +2,37 @@ import status from 'http-status';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
-  console.log(payload);
+  // check if the user exists
+  const isUserExist = await User.findOne({ id: payload?.id });
+
+  if (!isUserExist) {
+    throw new AppError(status.NOT_FOUND, 'This user is not found !');
+  }
+
+  // checking if  the  user is already deleted
+  const isDeleted = isUserExist?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(status.FORBIDDEN, 'This user is already deleted !');
+  }
+
+  // checking if the user is blocked
+  const userStatus = isUserExist?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(status.FORBIDDEN, 'This user is blocked !');
+  }
+
+  // checking if the password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    isUserExist?.password,
+  );
+
+  console.log(isPasswordMatched);
+
+  // Access Granted : Send Access Token and Refresh Token
 
   return {};
 };
