09d93429a7f2eb6485a61989a1d53d3ac39d950e
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-16 15:33:00 +0600

    17-6 Verify token using auth

--- ph-mission3/ph-university/src/app/middlwares/auth.ts
+++ ph-mission3/ph-university/src/app/middlwares/auth.ts
@@ -2,14 +2,28 @@ import { Request, Response, NextFunction } from 'express';
 import catchAsync from '../utils/CatchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
+    // if token does not exist
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not logged in !');
     }
-    next();
+
+    // check if token is valid
+    jwt.verify(token, config.jwt_access_secret as string, (err, decoded) => {
+      if (err) {
+        throw new AppError(httpStatus.UNAUTHORIZED, 'Invalid token !');
+      }
+
+      // decoded token
+      req.user = decoded as JwtPayload;
+      next();
+    });
   });
 };
 
