cbae1fb607f24ae2dfdd3bb86a08edb54f671f6e
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-02 20:07:43 +0600

    20-3 Add business logic validation into enrolled course service

--- ph-mission3/ph-university/src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ ph-mission3/ph-university/src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,5 +1,9 @@
+import AppError from '../../errors/AppError';
+import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
+import { Student } from '../student/student.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import EnrolledCourse from './enrolledCourse.model';
+import httpStatus from 'http-status';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
@@ -11,6 +15,38 @@ const createEnrolledCourseIntoDB = async (
    * Step3: create enrolled course
    *
    */
+
+  const { offeredCourse } = payload;
+  const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered course not found');
+  }
+
+  // check if the offered course max capacity is full
+  if (isOfferedCourseExists?.maxCapacity <= 0) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Offered course is full');
+  }
+
+  // check if the student exists
+
+  const student = await Student.findOne({ id: userId }, { _id: 1 });
+
+  if (!student) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Student not found');
+  }
+
+  // check if the student is already enrolled
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: student?._id,
+  });
+
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.CONFLICT, 'Student already enrolled');
+  }
+
   const result = await EnrolledCourse.create({});
   return result;
 };
