bce6f21f73b30c3764d6bc67aaea46485cbedc27
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-03 19:20:22 +0600

    21-2 Create data for academic faculty , academic department _ academic semester

--- ph-mission3/ph-university/src/app/modules/academicFaculty/academicFacalty.route.ts
+++ ph-mission3/ph-university/src/app/modules/academicFaculty/academicFacalty.route.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import validateRequest from '../../middlwares/validateRequest';
 import { AcademicFacultyValidation } from './academicFacalty.validation';
 import { AcademicFacultyControllers } from './academicFacalty.controller';
+import auth from '../../middlwares/auth';
+import { USER_ROLE } from '../user/user.contant';
 
 const router = express.Router();
 
 // create academic faculty route
 router.post(
   '/create-academic-faculty',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(AcademicFacultyValidation.createAcademicFacultyZodSchema),
   AcademicFacultyControllers.createAcademicFaculty,
 );
