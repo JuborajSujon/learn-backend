9fbbf03fc34e6336158b2d587bae39f6c6aa6866
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-04 15:17:59 +0600

    21-13 Apply custom pagination

--- ph-mission3/ph-university/src/app/modules/offeredCourse/offeredCourse.service.ts
+++ ph-mission3/ph-university/src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -144,7 +144,16 @@ const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {
   };
 };
 
-const getMyOfferedCoursesFromDB = async (userId: string) => {
+const getMyOfferedCoursesFromDB = async (
+  userId: string,
+  query: Record<string, unknown>,
+) => {
+  //pagination setup
+
+  const page = Number(query.page) || 1;
+  const limit = Number(query.limit) || 2;
+  const skip = (page - 1) * limit;
+
   const student = await Student.findOne({ id: userId });
 
   if (!student) {
@@ -165,7 +174,7 @@ const getMyOfferedCoursesFromDB = async (userId: string) => {
     );
   }
 
-  const result = await OfferedCourse.aggregate([
+  const aggregationQuery = [
     {
       $match: {
         semesterRegistration: currentOngoingRegistrationSemester?._id,
@@ -287,8 +296,34 @@ const getMyOfferedCoursesFromDB = async (userId: string) => {
         isPreRequisitesFulFilled: true,
       },
     },
+  ];
+
+  const paginationQuery = [
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+  ];
+  const result = await OfferedCourse.aggregate([
+    ...aggregationQuery,
+    ...paginationQuery,
   ]);
-  return result;
+
+  const total = (await OfferedCourse.aggregate(aggregationQuery)).length;
+
+  const totalPage = Math.ceil(result.length / limit);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+      totalPage,
+    },
+    result,
+  };
 };
 
 const getSingleOfferedCourseFromDB = async (id: string) => {
