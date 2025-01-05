2d2fda6e59b724919110b8464fa20198a38b9297
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-05 20:32:22 +0600

    23-8 Creating your first slice.

--- ph-mission4/explore-redux/src/redux/store.ts
+++ ph-mission4/explore-redux/src/redux/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
 
 export const reduxStore = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
