3cad3210c9624eb58f23e4091c05d9a994ee4d80
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-05 21:48:06 +0600

    24-2 Understanding mutation and ways to avoid it.

--- ph-mission4/explore-redux/test.js
+++ ph-mission4/explore-redux/test.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 // Pure function example
 function add(a, b) {
   return a + b;
@@ -15,3 +16,27 @@ function addTotal(amount) {
 
 console.log(addTotal(0));
 console.log(addTotal(1));
+
+// mutable function example
+
+const employee = {
+  name: "John Doe",
+  age: 30,
+  address: {
+    country: "USA",
+    city: "New York",
+  },
+};
+
+// const employee2 = employee; // point to the same object
+// employee.name = "Jane Doe";
+
+// const employee2 = { ...employee, name: "Jane Doe" };
+
+const employee2 = produce(employee, (draft) => {
+  draft.name = "Jane Doe";
+  draft.address.city = "San Francisco";
+});
+
+console.log(employee);
+console.log(employee2);
