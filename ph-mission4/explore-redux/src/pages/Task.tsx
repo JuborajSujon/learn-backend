fd85b2085c6e0360e428c7d34ed7fc710921d7a8
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-07 21:16:39 +0600

    24-11 Create modal using shadcn

--- ph-mission4/explore-redux/src/pages/Task.tsx
+++ ph-mission4/explore-redux/src/pages/Task.tsx
@@ -1,3 +1,4 @@
+import AddTaskModal from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { selectTasks } from "@/redux/features/tasks/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -6,8 +7,9 @@ const Task = () => {
   const tasks = useAppSelector(selectTasks);
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div>
+      <div className="flex justify-between items-center">
         <h1>Tasks</h1>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
         {tasks.map((task, index) => (
