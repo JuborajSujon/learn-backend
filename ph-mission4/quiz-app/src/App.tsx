a95105faaab91fc5430a4b7b5d6c9910ff6ec187
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-09 18:03:44 +0600

    CS Project setup

---
+++ ph-mission4/quiz-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Question from "./home/Question";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Question />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ ph-mission4/quiz-app/src/home/QuizControl.tsx
@@ -0,0 +1,3 @@
+export default function QuizControl() {
+  return <div>QuizControl</div>;
+}
