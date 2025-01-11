092084a287ea781141db0fb03c982df87e18bde3
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-11 11:55:04 +0600

    CS Part 3 Add question from with handler

--- ph-mission4/quiz-app-rtk/quiz-app-client/src/App.tsx
+++ ph-mission4/quiz-app-rtk/quiz-app-client/src/App.tsx
@@ -1,3 +1,4 @@
+import AddQuiz from "./home/AddQuiz";
 import AllQuiz from "./home/AllQuiz";
 import Question from "./home/Question";
 import QuizSummary from "./home/QuizSummary";
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="container mx-auto p-6">
       <h3 className="text-7xl font-bold text-center">Quiz App</h3>
+      <AddQuiz />
       <AllQuiz />
       <div className="flex flex-col items-center justify-center mt-5">
         {!quizComplete ? <Question /> : <QuizSummary />}
--- ph-mission4/quiz-app-rtk/quiz-app-client/src/redux/api/quizApi.ts
+++ ph-mission4/quiz-app-rtk/quiz-app-client/src/redux/api/quizApi.ts
@@ -3,13 +3,36 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const quizApi = createApi({
   reducerPath: "quizApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "http://localhost:5000/api",
   }),
+  tagTypes: ["Quiz"],
   endpoints: (builder) => ({
     getQuiz: builder.query({
-      query: () => "/api/quizzes",
+      query: () => "/quizzes",
+      providesTags: ["Quiz"],
+    }),
+    addQuiz: builder.mutation({
+      query: (body) => ({
+        url: `/quizzes`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Quiz"],
+    }),
+    updateQuiz: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/quizzes/${id}`,
+        method: "PATCH",
+        data,
+      }),
+    }),
+    deleteQuiz: builder.mutation({
+      query: (id) => ({
+        url: `/quizzes/${id}`,
+        method: "DELETE",
+      }),
     }),
   }),
 });
 
-export const { useGetQuizQuery } = quizApi;
+export const { useGetQuizQuery, useAddQuizMutation } = quizApi;
