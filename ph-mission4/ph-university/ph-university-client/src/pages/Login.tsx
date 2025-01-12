121780f34902ea1e6b48a9b3f68eeb0e0e7c4a9e
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-12 19:48:17 +0600

    27-5 Basic Login form and useLoginMutaion

--- ph-mission4/ph-university/ph-university-client/src/pages/Login.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/Login.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 export default function Login() {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-001",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data =>", data);
+  console.log("error =>", error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    login(userInfo);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" {...register("password")} />
+
+        <Button htmlType="submit">Login</Button>
+      </div>
+    </form>
+  );
 }
