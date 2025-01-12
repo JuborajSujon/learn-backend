bc8c01b9389600dfab909bb27553d6d668c7b8f6
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-12 11:40:49 +0600

    27-1 Refactor routes with util function

--- ph-mission4/ph-university-client/src/routes/admin.routes.tsx
+++ ph-mission4/ph-university-client/src/routes/admin.routes.tsx
@@ -5,17 +5,12 @@ import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 import { ReactNode } from "react";
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
 type TSidebarItem = {
   key: string;
   label: ReactNode;
   children?: TSidebarItem[];
 };
-const adminPaths = [
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -44,27 +39,27 @@ const adminPaths = [
 ];
 
 // Programatical  way
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.element) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
 
-  if (item.children) {
-    item.children.forEach((child) => {
-      if (child.path && child.element) {
-        acc.push({
-          path: child.path,
-          element: child.element,
-        });
-      }
-    });
-  }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       if (child.path && child.element) {
+//         acc.push({
+//           path: child.path,
+//           element: child.element,
+//         });
+//       }
+//     });
+//   }
 
-  return acc;
-}, []);
+//   return acc;
+// }, []);
 
 export const adminSidebarItems = adminPaths.reduce(
   (acc: TSidebarItem[], item) => {
