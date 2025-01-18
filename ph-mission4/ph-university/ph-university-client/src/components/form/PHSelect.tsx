7658f4f2dc9a22bc9834e92962e890e9bed598f5
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-18 21:25:32 +0600

    29-3 Reusable select element

--- ph-mission4/ph-university/ph-university-client/src/components/form/PHSelect.tsx
+++ ph-mission4/ph-university/ph-university-client/src/components/form/PHSelect.tsx
@@ -1,23 +1,25 @@
 import { Form, Select } from "antd";
+import { Controller } from "react-hook-form";
 
-const handleChange = (value: string) => {
-  console.log(`selected ${value}`);
+type TPHSelectProps = {
+  label: string;
+  name: string;
+  options: {
+    value: string;
+    label: string;
+    disabled?: boolean;
+  }[];
 };
 
-export default function PHSelect({ label }: { label: string }) {
+export default function PHSelect({ label, name, options }: TPHSelectProps) {
   return (
-    <Form.Item label={label}>
-      <Select
-        defaultValue="lucy"
-        style={{ width: "100%" }}
-        onChange={handleChange}
-        options={[
-          { value: "jack", label: "Jack" },
-          { value: "lucy", label: "Lucy" },
-          { value: "Yiminghe", label: "yiminghe" },
-          { value: "disabled", label: "Disabled", disabled: true },
-        ]}
-      />
-    </Form.Item>
+    <Controller
+      name={name}
+      render={({ field }) => (
+        <Form.Item label={label}>
+          <Select {...field} style={{ width: "100%" }} options={options} />
+        </Form.Item>
+      )}
+    />
   );
 }
--- ph-mission4/ph-university/ph-university-client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,20 +1,38 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
 import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 
+const nameOptions = [
+  {
+    value: "01",
+    label: "Autumn",
+  },
+  {
+    value: "02",
+    label: "Summer",
+  },
+  {
+    value: "03",
+    label: "Fall",
+  },
+];
+
 export default function CreateAcademicSemester() {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const semesterData = {
+      name: "Something",
+      code: "Something",
+    };
+
+    console.log(semesterData);
   };
+
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="Name" />
-          <PHInput type="text" name="year" label="Year" />
-          <PHSelect label="Name" />
+          <PHSelect name="name" label="Name" options={nameOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
