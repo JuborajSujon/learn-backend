d1b9677202cbd7ecd646f5582c87b188fd0dd780
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-19 22:27:00 +0600

    29-9 Resolve type errors of Redux Response

--- ph-mission4/ph-university/ph-university-client/src/redux/features/admin/academicManagement.api.ts
+++ ph-mission4/ph-university/ph-university-client/src/redux/features/admin/academicManagement.api.ts
@@ -1,3 +1,5 @@
+import { TresponseRedux } from "../../../types";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
 import { baseApi } from "../../api/baseApi";
 
 const academicManagementApi = baseApi.injectEndpoints({
@@ -7,8 +9,7 @@ const academicManagementApi = baseApi.injectEndpoints({
         url: "/academic-semesters",
         method: "GET",
       }),
-      transformResponse: (response) => {
-        console.log("inside redux", response);
+      transformResponse: (response: TresponseRedux<TAcademicSemester[]>) => {
         return {
           data: response.data,
           meta: response.meta,
