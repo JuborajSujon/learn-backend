718d76a5cf39a5fc8e97769a32ed607d07de4ffd
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-19 22:10:03 +0600

    29-8 Show semester data using Ant Design Table

--- ph-mission4/ph-university/ph-university-client/src/pages/admin/academicManagement/AcademicSemester.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,7 +1,89 @@
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 
+import React from "react";
+import { Table } from "antd";
+import type { TableColumnsType, TableProps } from "antd";
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
 export default function AcademicSemester() {
-  const data = useGetAllSemestersQuery(undefined);
-  console.log(data);
-  return <div>This is AcademicSemester component</div>;
+  const { data: semesterData } = useGetAllSemestersQuery(undefined);
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, name, year, startMonth, endMonth }) => {
+      return {
+        _id,
+        name,
+        year,
+        startMonth,
+        endMonth,
+      };
+    }
+  );
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+      filters: [
+        {
+          text: "Joe",
+          value: "Joe",
+        },
+        {
+          text: "Jim",
+          value: "Jim",
+        },
+        {
+          text: "Submenu",
+          value: "Submenu",
+          children: [
+            {
+              text: "Green",
+              value: "Green",
+            },
+            {
+              text: "Black",
+              value: "Black",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Year",
+      dataIndex: "year",
+    },
+    {
+      title: "Start Month",
+      dataIndex: "startMonth",
+    },
+    {
+      title: "End Month",
+      dataIndex: "endMonth",
+    },
+  ];
+
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+  return (
+    <Table<DataType>
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
+  );
 }
