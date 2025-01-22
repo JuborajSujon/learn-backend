034167a6cf3faa95318b239cd3a214460b188ded
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-22 13:04:10 +0600

    30-2 Create Student basic form structure

--- ph-mission4/ph-university/ph-university-client/src/pages/admin/userManagement/CreateStudent.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,21 +1,88 @@
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
+
+const studentData = {
+  password: "student123",
+  student: {
+    name: {
+      firstName: "I am ",
+      middleName: "Student",
+      lastName: "Number 1",
+    },
+    gender: "male",
+    dateOfBirth: "1990-01-01",
+    email: "student2@gmail.com",
+    contactNo: "1235678",
+    emergencyContactNo: "987-654-3210",
+    bloogGroup: "A+",
+    presentAddress: "123 Main St, Cityville",
+    permanentAddress: "456 Oak St, Townsville",
+    guardian: {
+      fatherName: "James Doe",
+      fatherOccupation: "Engineer",
+      fatherContactNo: "111-222-3333",
+      motherName: "Mary Doe",
+      motherOccupation: "Teacher",
+      motherContactNo: "444-555-6666",
+    },
+    localGuardian: {
+      name: "Alice Johnson",
+      occupation: "Doctor",
+      contactNo: "777-888-9999",
+      address: "789 Pine St, Villageton",
+    },
+    admissionSemester: "65b0104110b74fcbd7a25d92",
+    academicDepartment: "65b00fb010b74fcbd7a25d8e",
+  },
+};
 
 export default function CreateStudent() {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    const formData = new FormData();
-    formData.append("data", JSON.stringify(data));
-    console.log(Object.fromEntries(formData));
+    console.log(data);
+    // const formData = new FormData();
+    // formData.append("data", JSON.stringify(data));
+    // console.log(Object.fromEntries(formData));
     // console.log([...formData.entries()]);
   };
+
+  const formHeader = {
+    margin: "auto",
+    marginBottom: "20px",
+  };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" label="Name" />
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </PHForm>
+    <Row>
+      <h1 style={formHeader}>Create Student Form</h1>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit}>
+          <Divider>Personal Information</Divider>
+          <Row gutter={8}>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="gender" label="Gender" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="dateOfBirth" label="Date of Birth" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="bloogGroup" label="Blood Group" />
+            </Col>
+          </Row>
+
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 }
