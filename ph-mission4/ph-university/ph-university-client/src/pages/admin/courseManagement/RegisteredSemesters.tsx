c35d3a42032748bca04a6c8d2364162b144f730d
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-25 11:45:15 +0600

    31-1 Handle Semester Registration

---
+++ ph-mission4/ph-university/ph-university-client/src/pages/admin/courseManagement/RegisteredSemesters.tsx
@@ -0,0 +1,3 @@
+export default function RegisteredSemesters() {
+  return <div>RegisteredSemesters</div>;
+}
---
+++ ph-mission4/ph-university/ph-university-client/src/redux/features/admin/courseManagement.ts
@@ -0,0 +1,35 @@
+import { baseApi } from "../../api/baseApi";
+const courseManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // getAllRegisteredSemesters: builder.query({
+    //   query: (args) => {
+    //     const params = new URLSearchParams();
+    //     if (args) {
+    //       args.forEach((item: TQueryParam) => {
+    //         params.append(item.name, item.value as string);
+    //       });
+    //     }
+    //     return {
+    //       url: "/semester-registrations",
+    //       method: "GET",
+    //       params: params,
+    //     };
+    //   },
+    //   providesTags: ["semester"],
+    //   transformResponse: (response: TResponseRedux<TSemester[]>) => {
+    //     return {
+    //       data: response.data,
+    //       meta: response.meta,
+    //     };
+    //   },
+    // }),
+    addRegisteredSemester: builder.mutation({
+      query: (data) => ({
+        url: "/semester-registrations/create-semester-registration",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+export const { useAddRegisteredSemesterMutation } = courseManagementApi;
