219fe054cf0907044e9b55d915c798f93c056ae0
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-26 11:11:17 +0600

    32-5 Handle student offered course

--- ph-mission4/ph-university/ph-university-client/src/pages/student/OfferedCourse.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/student/OfferedCourse.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
 
 export default function OfferedCourse() {
+  const { data } = useGetAllOfferedCoursesQuery(undefined);
+  console.log({ data });
   return <div>OfferedCourse student</div>;
 }
