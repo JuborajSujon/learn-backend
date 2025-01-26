49d24cc688bfa976217ee42ec61e0e0149f3fdee
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-26 09:53:20 +0600

    32-2 Handle role based authentication

--- ph-mission4/ph-university/ph-university-client/src/pages/Login.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/Login.tsx
@@ -20,8 +20,8 @@ export default function Login() {
   // });
 
   const defaultValues = {
-    userId: "A-0001",
-    password: "admin123",
+    userId: "2024010001",
+    password: "student123",
   };
 
   const [login] = useLoginMutation();
