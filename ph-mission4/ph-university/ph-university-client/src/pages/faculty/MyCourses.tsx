cde5c9128d416a43a3b706aef5dae56a9bb63f69
Juboraj Sujon <mdsujon72@gmail.com>
2025-01-26 14:11:03 +0600

    32-11 Faculty Course filter options

--- ph-mission4/ph-university/ph-university-client/src/pages/faculty/MyCourses.tsx
+++ ph-mission4/ph-university/ph-university-client/src/pages/faculty/MyCourses.tsx
@@ -1,7 +1,39 @@
-import { useGetAllFacultiesQuery } from "../../redux/features/admin/userManagement.api";
+import { Button, Col, Flex } from "antd";
+import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
+import PHForm from "../../components/form/PHForm";
+import PHSelect from "../../components/form/PHSelect";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 export default function MyCourses() {
-  const { data } = useGetAllFacultiesQuery(undefined);
-  console.log(data);
-  return <div>MyCourses</div>;
+  const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery(undefined);
+  const navigate = useNavigate();
+  console.log(facultyCoursesData?.data);
+  const semesterOptions = facultyCoursesData?.data?.map((item: any) => ({
+    label: `${item?.academicSemester?.name} ${item.academicSemester.year}`,
+    value: item?.semesterRegistration?._id,
+  }));
+
+  const courseOptions = facultyCoursesData?.data?.map((item: any) => ({
+    label: item?.course?.title,
+    value: item?.course?._id,
+  }));
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    navigate(`/faculty/courses/${data.semesterRegistration}/${data.course}`);
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect
+            options={semesterOptions}
+            name="semesterRegistration"
+            label="Semester"
+          />
+          <PHSelect options={courseOptions} name="course" label="Course" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 }
