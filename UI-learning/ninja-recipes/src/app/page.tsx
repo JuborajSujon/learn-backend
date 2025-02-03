eec364a8ab1c3d32986f6c05c54f91253a5d61ef
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-03 13:02:10 +0600

    Shadcn UI Crash Course #2 - Card Components

--- UI-learning/ninja-recipes/src/app/page.tsx
+++ UI-learning/ninja-recipes/src/app/page.tsx
@@ -1,3 +1,50 @@
-export default function Home() {
-  return <main>Home</main>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface Recipe {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const res = await fetch("http://localhost:4000/recipes");
+  return res.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              {/*avater */}
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p className="text-green-600">Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
