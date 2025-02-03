ae3f4f3dffab1c1f7558d31a0bdd49d5bb8d8339
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-03 13:50:23 +0600

    Shadcn UI Crash Course #3 - Button Avater Badge

--- UI-learning/ninja-recipes/src/app/page.tsx
+++ UI-learning/ninja-recipes/src/app/page.tsx
@@ -7,6 +7,10 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
 interface Recipe {
   id: string;
   title: string;
@@ -29,7 +33,13 @@ export default async function Home() {
         {recipes.map((recipe) => (
           <Card key={recipe.id} className="flex flex-col justify-between">
             <CardHeader className="flex-row gap-4 items-center">
-              {/*avater */}
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} />
+                <AvatarFallback>
+                  {recipe.title.slice(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook.</CardDescription>
@@ -39,8 +49,8 @@ export default async function Home() {
               <p>{recipe.description}</p>
             </CardContent>
             <CardFooter className="flex justify-between">
-              <button>View Recipe</button>
-              {recipe.vegan && <p className="text-green-600">Vegan!</p>}
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant={"secondary"}>Vegan!</Badge>}
             </CardFooter>
           </Card>
         ))}
