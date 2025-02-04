fb4e313b21b2e43d0736adb1b0709cafa077319e
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-04 21:46:01 +0600

    Table with NextJS 14 & @shadcn UI - Part 8

--- UI-learning/dashboard-ui/src/app/page.tsx
+++ UI-learning/dashboard-ui/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Calendar from "@/components/Cards/Calendar";
 import General from "@/components/Cards/General";
+import { TableDemo } from "@/components/Cards/Table";
 import { Card } from "@/components/ui/card";
 
 export default function Home() {
@@ -7,15 +8,19 @@ export default function Home() {
     <div className="grid gap-8">
       <div className="grid grid-cols-2 gap-8">
         <General />
-        <div className="grid gap-8">
+        <div className="grid gap-8 overflow-hidden">
           <Calendar />
           <Calendar />
         </div>
       </div>
       <div className="grid grid-cols-3 gap-8">
-        <Card className="h-[400px]">hello world</Card>
-        <Card className="h-[400px]">hello world</Card>
-        <Card className="h-[400px]">hello world</Card>
+        <Card className="p-8 h-[300px] overflow-hidden">
+          <div className="h-full overflow-y-auto">
+            <TableDemo />
+          </div>
+        </Card>
+        <Card className="h-[300px]">hello world</Card>
+        <Card className="h-[300px]">hello world</Card>
       </div>
     </div>
   );
