a69a12a42979ccb757251e947d3963774c1481d4
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-05 11:27:23 +0600

    Shadcn Crash course with NextJS (chai ar code)

---
+++ UI-learning/chaiarshadcn/src/app/chai/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function ChaiPage() {
+  return (
+    <main className="h-full flex justify-center items-center flex-col">
+      <div>ChaiPage </div>
+      <div className="flex flex-col items-center">
+        <Button variant={"chai"}>Click Me</Button>
+      </div>
+    </main>
+  );
+}
---
+++ UI-learning/chaiarshadcn/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-full flex justify-center items-center">
+      <h1>Chai aur ShadCN</h1>
+    </main>
+  );
+}
