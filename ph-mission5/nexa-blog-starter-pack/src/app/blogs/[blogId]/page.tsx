3a0c22f7fba50ef3caca01c570ade691ede335d6
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-21 12:42:16 +0600

    36-4: Retrieve dynamic data with an ID for the detail page using SSR

---
+++ ph-mission5/nexa-blog-starter-pack/src/app/blogs/[blogId]/page.tsx
@@ -0,0 +1,22 @@
+import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+
+const BlogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+
+  return (
+    <div>
+      <BlogDetailsCard blog={blog} />
+    </div>
+  );
+};
+
+export default BlogDetailsPage;
