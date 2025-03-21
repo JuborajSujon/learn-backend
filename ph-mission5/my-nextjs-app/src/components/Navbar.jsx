489fe00445ff0d7b0845a6d68df5d6ee4ba150ed
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-21 10:53:09 +0600

    35-9: Handling Active Links in Next.js

--- ph-mission5/my-nextjs-app/src/components/Navbar.jsx
+++ ph-mission5/my-nextjs-app/src/components/Navbar.jsx
@@ -1,17 +1,92 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <div className="flex justify-between container px-6 mx-auto py-7 border-b">
-      <Link href="/">Logo</Link>
+      <div>
+        <Link
+          href="/"
+          className={
+            pathname === "/"
+              ? "text-blue-500"
+              : "cursor-pointer hover:underline"
+          }>
+          Logo
+        </Link>
+      </div>
 
-      <div className="flex gap-5">
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/counter">Counter</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/gallery">Gallery</Link>
+      <div>
+        <ul className="flex gap-5">
+          <li>
+            <Link
+              href="/about"
+              className={
+                pathname === "/about"
+                  ? "text-blue-500"
+                  : "cursor-pointer hover:underline"
+              }>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact"
+              className={
+                pathname === "/contact"
+                  ? "text-blue-500"
+                  : "cursor-pointer hover:underline"
+              }>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/counter"
+              className={
+                pathname === "/counter"
+                  ? "text-blue-500"
+                  : "cursor-pointer hover:underline"
+              }>
+              Counter
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/products"
+              className={
+                pathname === "/products"
+                  ? "text-blue-500"
+                  : "cursor-pointer hover:underline"
+              }>
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/posts"
+              className={
+                pathname === "/posts"
+                  ? "text-blue-500"
+                  : "cursor-pointer hover:underline"
+              }>
+              Posts
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/gallery"
+              className={
+                pathname === "/gallery"
+                  ? "text-blue-500"
+                  : "cursor-pointer hover:underline"
+              }>
+              Gallery
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
