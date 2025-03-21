5000f306c6903bd842800f41ebab0f0812a3f335
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-21 12:59:17 +0600

    36-5: Fetch dynamic data using generateStaticParams() for SSG

--- ph-mission5/nexa-blog-starter-pack/src/app/blogs/[blogId]/page.tsx
+++ ph-mission5/nexa-blog-starter-pack/src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,11 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blog = await res.json();
+  return blog.slice(0, 3).map((blog: Blog) => ({ blogId: blog.id }));
+};
 
 const BlogDetailsPage = async ({
   params,
