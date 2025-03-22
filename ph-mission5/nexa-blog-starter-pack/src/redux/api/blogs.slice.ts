683946f6e35588580c255ac8c0ddb7687a7238a4
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-22 10:19:21 +0600

    36-11: Displaying Blogs and Handling Loading with RTK Query. Module summary

--- ph-mission5/nexa-blog-starter-pack/src/redux/api/blogs.slice.ts
+++ ph-mission5/nexa-blog-starter-pack/src/redux/api/blogs.slice.ts
@@ -3,10 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: (build) => ({
     getBlogs: build.query({
-      query: () => `/blogs`,
+      query: () => "/blogs",
     }),
   }),
 });
