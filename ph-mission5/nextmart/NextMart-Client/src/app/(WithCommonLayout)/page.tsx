6948a4ced13fd5c7dff82a54fad2466d6bb5114f
Juboraj Sujon <mdsujon72@gmail.com>
2025-03-26 21:29:30 +0600

    39-3: Setting Up User and Admin Layouts with Route Groups

--- ph-mission5/nextmart/NextMart-Client/src/app/(WithCommonLayout)/page.tsx
+++ ph-mission5/nextmart/NextMart-Client/src/app/(WithCommonLayout)/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 const HomePage = () => {
   return (
     <div>
-      <Button>Click Me</Button>
+      <h1>NextMart Home Page</h1>
     </div>
   );
 };
