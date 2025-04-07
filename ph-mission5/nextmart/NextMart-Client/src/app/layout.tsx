e1e666b2b3b127765eb7e0447b62172bd26f2cb4
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-07 12:03:46 +0600

    41-1: Revalidate Categories & Brands Data

--- ph-mission5/nextmart/NextMart-Client/src/app/layout.tsx
+++ ph-mission5/nextmart/NextMart-Client/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <Providers>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.className} antialiased`}
           data-new-gr-c-s-check-loaded="14.1229.0"
           data-gr-ext-installed=""
           cz-shortcut-listen="true">
